6d0717407c303e4c117185eb19e63972ee525793
Luke Fraser <lukefrasera@gmail.com>
2015-11-29 21:21:23 -0800

    providing example code

--- scripts/examples/motor_control_demo.py
+++ scripts/examples/motor_control_demo.py
@@ -1,11 +1,58 @@
 #!/usr/bin/env python
 
+class _Getch:
+  '''
+  Gets a single character from standard input.  Does not echo to the screen.
+  '''
+  def __init__(self):
+    try:
+      self.impl = _GetchWindows()
+    except ImportError:
+      self.impl = _GetchUnix()
+
+  def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+  def __init__(self):
+    import tty, sys
+
+  def __call__(self):
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if ch == '\x03':
+      raise KeyboardInterrupt
+    return ch
+
+
+class _GetchWindows:
+  def __init__(self):
+    import msvcrt
+
+  def __call__(self):
+    import msvcrt
+    return msvcrt.getch()
+
 #from drivers import motor
 '''
 Show the use of the Motor driver classes
 '''
 def main():
-  print "hello"
+  get_char = _Getch()
+  while True:
+    ch = get_char()
+    if ch:
+      if ch == 'l':
+        print "You Pressed L"
+      else:
+        print "you didn't press L"
+
 
 
 if __name__ == '__main__':
