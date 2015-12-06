b8dc04aa757268c349ca4dc5268453b3daead315
Luke Fraser <lukefrasera@gmail.com>
2015-12-06 15:13:05 -0800

    adding new run script procedures

--- utils/run.sh
+++ utils/run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo ${DIR}/../scripts/examples/system_architecture.py >&2
+sudo ../scripts/examples/system_architecture.py
