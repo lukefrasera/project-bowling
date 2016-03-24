86e9b3c6f4640f3d4558bd3bb0d1c4338cdd80fc
Luke Fraser <lukefrasera@gmail.com>
2016-03-24 10:22:57 -0700

    fixing x11 qt application

--- drivers/application/application.py
+++ drivers/application/application.py
@@ -5,48 +5,86 @@ import time
 import signal
 from tools import get_char as _getch
 import sys
-class Program:
+
+from PyQt4.QtGui import QWidget, QApplication
+
+class Program(QWidget):
   def __init__(self):
-    self.running = True
+    super(Program, self).__init__()
+    self.setGeometry(300, 300, 250, 150)
+    self.setWindowTitle('Program')
+    self.setMouseTracking(True)
+  def Start(self):
+    self.show()
+  def keyPressEvent(self, e):
+    if not e.isAutoRepeat():
+      self.KeyPressEvent(e)
 
   def KeyPressEvent(self, e):
     pass
+  def keyReleaseEvent(self, e):
+    if not e.isAutoRepeat():
+      self.KeyReleaseEvent(e)
 
   def KeyReleaseEvent(self, e):
     pass
 
-class App(object):
-  def __init__(self, program):
-    self.program = program
 
-    signal.signal(signal.SIGINT, self.SignalHandle)
-    self.hooks = HookManager()
-    self.hooks.KeyDown = self.program.KeyPressEvent
-    self.hooks.KeyUp = self.program.KeyReleaseEvent
-    self.hooks.HookKeyboard()
-    self.hooks.start()
+class App(QApplication):
+  def __init__(self, args):
+    super(App, self).__init__(args)
 
-  def Kill(self):
-    self.hooks.cancel()
-  def MainLoop(self):
-    getch = _getch._Getch()
-    while self.program.running:
-      time.sleep(0.1)
-      c = getch()
-    print "Closing the App"
-    self.Kill()
-  def Start(self):
-    self.MainLoop()
+  def Wait(self):
+    sys.exit(self.exec_())
 
-  def SignalHandle(self, signal, frame):
-    print "Ctrl-C: Ending Program"
-    self.hooks.cancel()
-    sys.exit(0)
+
+# class Program:
+#   def __init__(self):
+#     self.running = True
+
+#   def KeyPressEvent(self, e):
+#     pass
+
+#   def KeyReleaseEvent(self, e):
+#     pass
+
+# class App(object):
+#   def __init__(self, program):
+#     self.program = program
+
+#     signal.signal(signal.SIGINT, self.SignalHandle)
+#     self.hooks = HookManager()
+#     self.hooks.KeyDown = self.program.KeyPressEvent
+#     self.hooks.KeyUp = self.program.KeyReleaseEvent
+#     self.hooks.HookKeyboard()
+#     self.hooks.start()
+
+#   def Kill(self):
+#     self.hooks.cancel()
+#   def MainLoop(self):
+#     getch = _getch._Getch()
+#     while self.program.running:
+#       time.sleep(0.1)
+#       c = getch()
+#     print "Closing the App"
+#     self.Kill()
+#   def Start(self):
+#     self.MainLoop()
+
+#   def SignalHandle(self, signal, frame):
+#     print "Ctrl-C: Ending Program"
+#     self.hooks.cancel()
+#     sys.exit(0)
+
+
 
 def main():
+  app = App(sys.argv)
+  # app = QApplication(sys.argv)
   test = Program()
-  a = App(test)
-  a.Start()
+  # test.show()
+  test.Start()
+  app.Wait()
 
 if __name__ == '__main__':
   main()
