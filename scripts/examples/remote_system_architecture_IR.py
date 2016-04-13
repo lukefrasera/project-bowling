5175e3833680b399c9299b9f9030fd54a02b9955
"Luke Fraser" <lukefrasera@gmail.com>
2016-04-13 12:46:09 -0700

    adding serial flush command

--- scripts/examples/remote_system_architecture_IR.py
+++ scripts/examples/remote_system_architecture_IR.py
@@ -97,6 +97,10 @@ def main():
   print "Program Ready!"
   # Main loop
   ser = serial.Serial('/dev/ttyUSB0', 9600)
+  # flush serial buffer so that all previous commands are removed
+  ser.flush() 
+  time.sleep(.1) # wait for the buffer to be freed
+
   RPIO.setup(17,RPIO.IN, pull_up_down = RPIO.PUD_DOWN)
   ###Calibration - new
   loweringtime = 0
